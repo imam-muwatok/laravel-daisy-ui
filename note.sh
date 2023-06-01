d601adc6d5239165bab8dca9eb4823a92fb39d33
imam-muwatok <muwatok123@gmail.com>
2023-06-01 19:49:54 +0700

    first commit

---
+++ note.sh
@@ -0,0 +1,16 @@
+#laravel daisyUI
+
+composer require laravel/breeze
+
+php artisan breeze:install blade
+
+npm install
+
+run dev
+
+npm i -D daisyui
+
+#add pada plugin config tailwind
+require('daisyui')
+
+npm install && run dev
